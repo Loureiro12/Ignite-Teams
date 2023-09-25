fd6d008509888e3fe78e45da905d526046ee1268
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2023-09-25 01:14:59 -0300

    feat: initial project configuration

--- App.tsx
+++ App.tsx
@@ -1,5 +1,20 @@
+import { ActivityIndicator } from "react-native";
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 import { Groups } from "@screens/Groups";
+import theme from "@theme/index";
 
 export default function App() {
-  return <Groups />;
+  const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {fontLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
+  );
 }
