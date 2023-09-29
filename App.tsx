dbb2b34cf3c7425abfb38ffa2691ba7acb785fcd
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2023-09-28 21:40:57 -0300

    feat: Loading Component

--- App.tsx
+++ App.tsx
@@ -6,15 +6,17 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
+
+import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
