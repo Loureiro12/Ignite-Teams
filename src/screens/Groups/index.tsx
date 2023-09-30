5897d705bba93fcd5d97f8796bb95b1ea469c490
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2023-09-30 06:46:44 -0300

    feat: creating listing component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,13 +3,25 @@ import { Highlight } from "@components/Highlight";
 
 import { Container } from "./styles";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da escola", "Fut"]);
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera da escola"/>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 }
