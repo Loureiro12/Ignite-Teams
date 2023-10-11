44bc1fa43b6d7e4f37f8739d5b74b37b1ba52af0
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2023-10-10 23:12:48 -0300

    feat: finishing creation of the screens

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
@@ -6,3 +6,30 @@ export const Container = styled.View`
 
   padding: 24px;
 `;
+
+export const Form = styled.View`
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 6px;
+`;
+
+export const HeaderList = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  align-items: center;
+
+  margin: 32px 0px 12px;
+`
+
+export const NumberOfPlayers = styled.Text`
+ ${({ theme }) =>
+    css`
+      font-family: ${theme.FONT_FAMILY.BOLD};
+      font-size: ${theme.FONT_SIZE.SM}px;
+      color: ${theme.COLORS.GRAY_200};
+    `};
+`
